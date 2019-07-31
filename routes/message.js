fc95e99024262e32b38725f358e3ed64bbb4b27e
Eder Marques <eder.marques@citi.org.br>
2019-07-31 16:51:13 -0300

    delete firebase

--- routes/message.js
+++ routes/message.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const db = require('../providers/firebase');
+
+const Message = db.ref(`${process.env.FIREBASE_ACCESS_TOKEN}/messages`);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('msg');
+// get messages from this chat
+app.get('/:chatID', async (req, res) => {
+  const messagesFirebase = await Message.child(req.params.chatID).once('value');
+  const messages = [];
+  messagesFirebase.forEach((message) => {
+    messages.push(message.val());
+  });
+  res.send(messages);
+});
+
+// add message to this chat
+app.post('/:chatID', async (req, res) => {
+  await Message.child(req.params.chatID).push({
+    sender: req.session.userID,
+    content: req.body.content,
+    timestamp: new Date().getTime(),
+  });
+  res.send('ok');
 });
 
 module.exports = app;
--- views/home/home.js
+++ views/home/home.js
@@ -10,6 +10,31 @@ gel('#add-contact-form').addEventListener('submit', async (e) => {
   }
 });
 
+gel('#send-message-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  await axios.post(`/api/message/${window.localStorage.getItem('chatID')}`, {
+    content: gel('input[name=message]').value,
+  });
+});
+
+const renderMessages = async (contactID) => {
+  const users = [contactID, window.localStorage.getItem('userID')];
+  users.sort();
+  const chatIdRaw = users.join('(*-*)');
+  const chatID = btoa(chatIdRaw);
+
+  window.localStorage.setItem('chatID', chatID);
+
+  const messages = await axios.get(`/api/message/${chatID}`);
+  
+  gel('#messages').innerHTML = '';
+  messages.data.forEach((message) => {
+    gel('#messages').innerHTML += `
+      <p>${message.sender}: ${message.content} <${message.timestamp}></p>
+    `;
+  });
+}
+
 
 setInterval(async () => {
   const response = await axios.get('/api/user/contact?tamanho=1');
@@ -19,7 +44,7 @@ setInterval(async () => {
     gel('#contacts').innerHTML = '';
     contacts.data.forEach((contact) => {
       gel('#contacts').innerHTML += `
-        <p>${contact.alias}</p>
+        <p onclick="renderMessages('${contact.id}')">${contact.alias}</p>
       `;
     });
   } 
